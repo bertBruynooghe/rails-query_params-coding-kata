3b9be97a2de494121b4e4cabde68116ffaebb719
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-04-07 21:28:59 +0200

    fixed hidden_field_tag for fixnum values

--- config/initializers/form_tag_helper.rb
+++ config/initializers/form_tag_helper.rb
@@ -2,13 +2,12 @@ module ActionView
   module Helpers
     module FormTagHelper
       def hidden_field_tag_with_hash_support(name, value = nil, options = {})
-        case value
-        when String
-          hidden_field_tag_without_hash_support(name, value, options)
-        when Hash
+        if value.is_a?(Hash)
             value.reduce('') do |c, (k, v)|
               c << hidden_field_tag_without_hash_support("#{name}[#{k}]", v, options)
             end.html_safe
+        else
+          hidden_field_tag_without_hash_support(name, value, options)
         end
       end
       alias_method_chain :hidden_field_tag, :hash_support
