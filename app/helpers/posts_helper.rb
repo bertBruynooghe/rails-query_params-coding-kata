f098e0ff2a83fdeaa9259d70f9402b02cd6db0c7
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-04-08 10:36:00 +0200

    introduced query_params class

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,19 +1,6 @@
 module PostsHelper
   # create a new params object based on the current params with some alterations
-  def posts_params(current_params, new_params)
-    params = current_params.deep_dup
-    new_params.reduce(params) do |result, (k,v)|
-      send("posts_sanitize_for_#{k}", result, v) if respond_to?("posts_sanitize_for_#{k}")
-      params[k] = v
-    end
-    params
-  end
-
-  def posts_sanitize_for_title_starting_with(params, _)
-    params[:page] = 1
-  end
-
-  def posts_sanitize_for_reorder(params, _)
-    params[:page] = 1
+  def posts_params(current_params, overrides)
+    PostsQueryParams.new(current_params, overrides).params
   end
 end
