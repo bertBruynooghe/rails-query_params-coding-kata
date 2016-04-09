0bc44b2c6b0496f37d03f6e38f2c971b364122f4
Bert Bruynooghe <bert.bruynooghe@up-nxt.com>
2016-04-09 11:25:16 +0200

    reset to original situation

---
+++ app/models/posts_params.rb
@@ -0,0 +1,17 @@
+class PostsParams
+  # include ActiveModel::Validations
+  #
+  # attr_reader :errors
+  #
+  # validates_numericality_of :page, only_integer: true, greater_than: 0
+
+  # def title_starting_with=(value)
+  #   self[:page] = 1
+  #   self[:title_starting_with] = value
+  # end
+  #
+  # def reorder=(value)
+  #   self[:page] = 1
+  #   self[:reorder] = value
+  # end
+end
